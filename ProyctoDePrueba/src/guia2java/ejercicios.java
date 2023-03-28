1657ef0aa92c899dd0dde8aef22f18ffa194d85c
PaulaDomínguez <pauladominguez1990@gmail.com>
2023-03-28 13:10:46 -0300

    Terminado:Guia 2

--- ProyctoDePrueba/src/guia2java/ejercicios.java
+++ ProyctoDePrueba/src/guia2java/ejercicios.java
@@ -9,19 +9,62 @@ public class ejercicios {
         
     /*Escribir un programa que pida dos números enteros por teclado y calcule la suma de los dos.
     El programa deberá después mostrar el resultado de la suma */
-        
-        Scanner leer = new Scanner(System.in);
-        int nro1;
-        int nro2;
-        int suma;
-        System.out.println("Ingrese 1er nro ");
-        //nro1 = leer.next();
-        
-        System.out.println("Ingrese el 2do nro");
-        //nro2 = leer.next();
-        //int suma = nro1 + nro2;
-        
-        
+       
+    int nro1, nro2, suma;
+    Scanner entrada = new Scanner(System.in);
+    System.out.println("Ingrese el primer nro");
+    nro1=entrada.nextInt();
+    System.out.println("Ingrese el segundo nro");
+    nro2=entrada.nextInt();
+    suma= nro1 + nro2;
+    System.out.println("La suma de los nros resulta: " + suma);
+    
+    //Escribir un programa que pida tu nombre, lo guarde en una variable y lo muestre por pantalla.
+    
+    Scanner entrada2 = new Scanner(System.in);
+    String nombre;
+    System.out.println("Ingrese su nombre ");
+    nombre=entrada2.next();
+    System.out.println("Hola "+nombre+" ,Bienvenida!");
+    
+    
+    /*Escribir un programa que pida una frase y la muestre toda en mayúsculas y después toda en minúsculas. 
+    Nota: investigar la función toUpperCase() y toLowerCase() en Java.*/
+    
+    Scanner entrada3 = new Scanner(System.in);
+    String frase;
+    System.out.println("Ingrese una frase ");
+    frase = entrada3.nextLine();
+    
+    System.out.println("La frase ingresada es " + frase.toUpperCase());
+    System.out.println("La frase ingresada es " + frase.toLowerCase());
+    
+    
+    /*Dada una cantidad de grados centígrados se debe mostrar su equivalente en grados Fahrenheit.
+    La fórmula correspondiente es: F = 32 + (9 * C / 5).*/
+    
+    
+    Scanner entrada4= new Scanner(System.in);
+    double Ct,F;
+    System.out.println("Ingrese los grados Celcius");
+    Ct=entrada4.nextDouble();
+    F= 32 + (9 *Ct/5);
+    System.out.println("El equivalente en Grados Fahrenheit es: " + F);
+    
+    
+    /*Escribir un programa que lea un número entero por teclado y muestre por pantalla el doble,
+    el triple y la raíz cuadrada de ese número. 
+    Nota: investigar la función Math.sqrt().*/
+    
+    Scanner entrada5= new Scanner(System.in);
+    double num,doble,triple,raiz;
+    System.out.println("Ingrese un nro ");
+    num=entrada5.nextDouble();
+    doble= num*2;
+    triple= num*3;
+    raiz= Math.sqrt(num);
+    System.out.println("El doble del nro es: "+ doble+" El triple es: "+triple+" La raíz cuadrada es: "+raiz);
+            
     }   
     
 }
